e4052fd7725121b029f7b95a5333728fee617fdf
unknown <bunt88@gmail.com>
2022-09-25 15:52:14 -0600

    Fixed error with single user

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,30 +1,19 @@
-const userRouter = require('express').Router();
+const userRouter = require("express").Router();
 
 const {
-    getAllUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    addFriend,
-    deleteUser,
-    deleteFriend
-} = require('../../controllers/user-controller');
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  addFriend,
+  deleteUser,
+  deleteFriend,
+} = require("../../controllers/user-controller");
 
-userRouter
-    .route('/')
-    .get(getAllUsers)
-    .post(createUser);
+userRouter.route("/").get(getAllUsers).post(createUser);
 
+userRouter.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
-    userRouter  
-    .route('/:id')
-    .get(getUserById)
-    .put(updateUser)
-    .delete(deleteUser);
-
-    userRouter  
-    .route('/:id/friends/:friendId')
-    .put(addFriend)
-    .delete(deleteFriend)
+userRouter.route("/:id/friends/:friendId").put(addFriend).delete(deleteFriend);
 
 module.exports = userRouter;
