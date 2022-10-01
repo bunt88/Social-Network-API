32c0be7b84da923bb654cd4a6ff8948ef9f22ba6
unknown <bunt88@gmail.com>
2022-10-01 15:08:32 -0600

    Fixed Server Error

--- models/Thought.js
+++ models/Thought.js
@@ -1,66 +1,70 @@
-const { Schema, model, Types } = require('mongoose');
-const moment = require('moment');
+const { Schema, model, Types } = require("mongoose");
+const moment = require("moment");
 
-const ReactionSchema = new Schema({
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-        },
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    {
-        toJSON: {
-            getters: true
-        }
-});
-
-const ThoughtSchema = new Schema({
-        thoughtText: {
-            type: String,
-            required: true,
-            maxlength: 280,
-            minlength: 1
-        },
-
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-        },
-
-        username: {
-            type: String,
-            required: true
-        },
-        reactions : [ReactionSchema]
-
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
     },
-    {
-        toJSON: {
-        virtuals: true,
-        getters: true
-        },
-        id: false
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reaction?.length;
 });
 
-ThoughtSchema.virtual('reactionCount').get(function() {
-    return this.reaction.length;
-});
-
-const Thought = model('Thought', ThoughtSchema);
+const Thought = model("Thought", ThoughtSchema);
 
 module.exports = Thought;
